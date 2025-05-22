2c796452a472a2a8ea319875ce76a10d63196b48
Luis Gil <luisggil01@gmail.com>
2025-05-22 13:32:27 -0400

    No se estaba Editanto el Id en el Admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
-# Verifica el nombre exacto del modelo, si es Produccion en tu models.py cambia acá también
 from .models import ProduccionTecnico
-from tecnicos.models import Curso
 
 
 @admin.register(ProduccionTecnico)
 class ProduccionTecnicoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'tecnico', 'status',
-                    'fecha_aprobacion', 'monto')
-    list_filter = ('status', 'fecha_aprobacion')
-    search_fields = ('id', 'tecnico__user__username', 'descripcion')
-    ordering = ('-fecha_aprobacion',)
-    readonly_fields = ('id',)
+    list_display = ('tecnico', 'id', 'status',
+                    'fecha_aprobacion', 'descripcion', 'monto')
+    fields = ('tecnico', 'id', 'status',
+              'fecha_aprobacion', 'descripcion', 'monto')
--- dashboard/models.py
+++ dashboard/models.py
@@ -3,20 +3,19 @@ from django.contrib.auth.models import User
 
 
 class ProduccionTecnico(models.Model):
-    STATUS_CHOICES = [
+    ESTADOS = [
         ('pendiente', 'Pendiente'),
         ('aprobado', 'Aprobado'),
         ('rechazado', 'Rechazado'),
     ]
 
-    id = models.IntegerField(primary_key=True, verbose_name="ID Manual")
-    tecnico = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='producciones')
-    status = models.CharField(
-        max_length=10, choices=STATUS_CHOICES, default='pendiente')
-    fecha_aprobacion = models.DateField(blank=True, null=True)
-    descripcion = models.TextField()
-    monto = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    tecnico = models.ForeignKey(User, on_delete=models.CASCADE)
+    id = models.CharField(max_length=100, unique=True, primary_key=True)
+    status = models.CharField(max_length=20, choices=ESTADOS)
+    fecha_aprobacion = models.DateField(null=True, blank=True)
+    descripcion = models.TextField(blank=True)
+    monto = models.DecimalField(
+        max_digits=10, decimal_places=2)  # Monto a cobrar
 
     def __str__(self):
-        return f"{self.id} - {self.tecnico.username}"
+        return f"{self.tecnico} - {self.id}"
