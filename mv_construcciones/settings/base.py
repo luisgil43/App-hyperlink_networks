0c98a0e926efc696395a086afbb7bd9a1dcb9e54
Luis Gil <luisggil01@gmail.com>
2025-05-23 18:59:57 -0400

    se repara ya que no se estaba viendo las liquidaciones

--- mv_construcciones/settings/base.py
+++ mv_construcciones/settings/base.py
@@ -89,11 +89,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Archivos multimedia (si usas Cloudinary)
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-}
+# CLOUDINARY_STORAGE = {
+# 'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+# 'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+# 'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+# }
 
 # Correo electrónico
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
