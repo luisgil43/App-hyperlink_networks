5a6b8ac99dbfc3702376adbe4664eb3c024960f8
Luis Gil <luisggil01@gmail.com>
2025-05-23 11:31:56 -0400

    Se agrega mensaje de error para cuando el usuario este creando dos liquidaciones en el mismo mes año y el mismo usuario

--- liquidaciones/admin.py
+++ liquidaciones/admin.py
@@ -1,5 +1,6 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.utils.html import format_html
+from django.db import IntegrityError
 from .models import Liquidacion
 
 
@@ -14,3 +15,14 @@ class LiquidacionAdmin(admin.ModelAdmin):
             return format_html('<a href="{}" target="_blank">Descargar PDF Firmado</a>', obj.pdf_firmado.url)
         return "-"
     ver_pdf_firmado.short_description = "PDF Firmado"
+
+    def save_model(self, request, obj, form, change):
+        try:
+            super().save_model(request, obj, form, change)
+        except IntegrityError:
+            self.message_user(
+                request,
+                f"⚠️ Estas intentando agregarle una liquidación a '{obj.tecnico}' en el mes '{obj.mes}' del año '{obj.año}', "
+                f"pero ya existe una registrada. Si necesitas cambiarla, edita o elimina la que ya está.",
+                level=messages.ERROR
+            )
