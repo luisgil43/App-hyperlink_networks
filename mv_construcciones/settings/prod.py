e1911dadea30c9f669b16ed37d56f8faf3b20a79
Luis Gil <luisggil01@gmail.com>
2025-05-23 20:45:49 -0400

    Cambio para que me salga bien la informacion en el admin

--- mv_construcciones/settings/prod.py
+++ mv_construcciones/settings/prod.py
@@ -1,10 +1,22 @@
 from .base import *
+import os
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['app-mv.onrender.com']
 
-# Seguridad recomendada para producción
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# ✅ Usa WhiteNoise para servir archivos estáticos
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Seguridad para producción
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
