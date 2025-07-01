e69eb0daeb1ece3ed699feae5515c856430c1e10
Luis Gil <luisggil01@gmail.com>
2025-07-01 11:31:33 -0400

    Crear roles y admin automáticamente en producción

--- usuarios/apps.py
+++ usuarios/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
-# from simple_history import register
 
 
 class UsuariosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'usuarios'
-    
+
     def ready(self):
+        # Solo activamos las señales, el resto va en signals.py
         import usuarios.signals
