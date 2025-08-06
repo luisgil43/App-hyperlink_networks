e4ae77d93ab4b4114a8596abce6dee9a73d11160
Luis Gil <luisggil01@gmail.com>
2025-08-05 20:08:31 -0400

    login

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -19,7 +19,8 @@ urlpatterns = [
          views.resetear_contraseña, name='resetear_contraseña'),
     path('recuperar/enviado/', TemplateView.as_view(
         template_name='usuarios/confirmacion_envio.html'), name='confirmacion_envio'),
-    path('login/', views.login_unificado, name='login'),
+    # path('login/', views.login_unificado, name='login'),
+    path('login/', views.login_unificado, name='login_unificado'),
     path('seleccionar-rol/', views.seleccionar_rol, name='seleccionar_rol'),
     path('notificacion/<int:pk>/leer/',
          views.marcar_notificacion_como_leida, name='leer_notificacion'),
