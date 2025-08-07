93593657a7791fd22a5f195243cc9f60a5a1fb11
Luis Gil <luisggil01@gmail.com>
2025-08-07 17:08:17 -0400

    Eliminacion de cosas en el model

--- facturacion/forms.py
+++ facturacion/forms.py
@@ -5,116 +5,12 @@ from decimal import Decimal
 from operaciones.templatetags.custom_filters import formato_clp  # Usa tu filtro de CLP
 from .models import TipoGasto
 from .models import CartolaMovimiento, TipoGasto, Proyecto
-from .models import FacturaOC
 from django import forms
-from .models import OrdenCompraFacturacion
 
 
 # facturacion/forms.py
 
 from django import forms
-from .models import OrdenCompraFacturacion
-
-
-class OrdenCompraFacturacionForm(forms.ModelForm):
-    class Meta:
-        model = OrdenCompraFacturacion
-        fields = [
-            'du',
-            'orden_compra',
-            'pos',
-            'cantidad',
-            'unidad_medida',
-            'material_servicio',
-            'descripcion_sitio',
-            'fecha_entrega',
-            'precio_unitario',
-            'monto',
-        ]
-        widgets = {
-            'fecha_entrega': forms.DateInput(
-                attrs={
-                    'type': 'date',
-                    'class': 'campo-formulario w-full'
-                },
-                format='%Y-%m-%d'  # 👈 Agrega este formato
-            ),
-            'cantidad': forms.NumberInput(attrs={'step': 'any', 'class': 'campo-formulario w-full'}),
-            'precio_unitario': forms.NumberInput(attrs={'step': 'any', 'class': 'campo-formulario w-full'}),
-            'monto': forms.NumberInput(attrs={'step': 'any', 'class': 'campo-formulario w-full'}),
-            'du': forms.Select(attrs={'class': 'campo-formulario w-full'}),
-            'orden_compra': forms.TextInput(attrs={'class': 'campo-formulario w-full'}),
-            'pos': forms.TextInput(attrs={'class': 'campo-formulario w-full'}),
-            'unidad_medida': forms.TextInput(attrs={'class': 'campo-formulario w-full'}),
-            'material_servicio': forms.TextInput(attrs={'class': 'campo-formulario w-full'}),
-            'descripcion_sitio': forms.Textarea(attrs={'class': 'campo-formulario w-full', 'rows': 2}),
-        }
-        labels = {
-            'du': 'DU',
-            'orden_compra': 'Orden de Compra',
-            'pos': 'POS',
-            'cantidad': 'Cantidad',
-            'unidad_medida': 'UM',
-            'material_servicio': 'Material/Servicio',
-            'descripcion_sitio': 'Descripción Sitio',
-            'fecha_entrega': 'Fecha Entrega',
-            'precio_unitario': 'Precio Unitario',
-            'monto': 'Monto',
-        }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # 👇 Esto es clave para precargar bien la fecha
-        if self.instance and self.instance.fecha_entrega:
-            self.initial['fecha_entrega'] = self.instance.fecha_entrega.strftime(
-                '%Y-%m-%d')
-
-
-class ImportarFacturasForm(forms.Form):
-    archivo = forms.FileField(
-        label="Archivo Excel",
-        widget=forms.ClearableFileInput(
-            attrs={"class": "border rounded-lg px-3 py-2"})
-    )
-
-
-class FacturaOCForm(forms.ModelForm):
-    class Meta:
-        model = FacturaOC
-        fields = [
-            'hes',
-            'valor_en_clp',
-            'conformidad',
-            'num_factura',
-            'fecha_facturacion',
-            'factorizado',
-            'fecha_factoring',
-            'cobrado',
-        ]
-        widgets = {
-            'hes': forms.TextInput(attrs={'class': 'w-full border rounded-xl px-3 py-2'}),
-            'valor_en_clp': forms.NumberInput(attrs={'class': 'w-full border rounded-xl px-3 py-2'}),
-            'conformidad': forms.TextInput(attrs={'class': 'w-full border rounded-xl px-3 py-2'}),
-            'num_factura': forms.TextInput(attrs={'class': 'w-full border rounded-xl px-3 py-2'}),
-            'fecha_facturacion': forms.DateInput(
-                format='%Y-%m-%d',
-                attrs={'type': 'date',
-                       'class': 'w-full border rounded-xl px-3 py-2'}
-            ),
-            'factorizado': forms.CheckboxInput(attrs={'class': 'h-4 w-4'}),
-            'fecha_factoring': forms.DateInput(
-                format='%Y-%m-%d',
-                attrs={'type': 'date',
-                       'class': 'w-full border rounded-xl px-3 py-2'}
-            ),
-            'cobrado': forms.CheckboxInput(attrs={'class': 'h-4 w-4'}),
-        }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Para que Django use el formato correcto en valores iniciales
-        self.fields['fecha_facturacion'].input_formats = ['%Y-%m-%d']
-        self.fields['fecha_factoring'].input_formats = ['%Y-%m-%d']
 
 
 class CartolaAbonoForm(forms.ModelForm):
--- operaciones/models.py
+++ operaciones/models.py
@@ -6,173 +6,26 @@ from usuarios.models import CustomUser
 from utils.paths import upload_to  # 👈 Importamos upload dinámico
 
 
-class SitioMovil(models.Model):
-    id_sites = models.CharField(max_length=100, unique=True)
-    id_claro = models.CharField(max_length=100, blank=True, null=True)
-    id_sites_new = models.CharField(max_length=100, blank=True, null=True)
-    region = models.CharField(max_length=100, blank=True, null=True)
-    nombre = models.CharField(max_length=255, blank=True, null=True)
-    direccion = models.CharField(max_length=255, blank=True, null=True)
-
-    latitud = models.FloatField(blank=True, null=True)
-    longitud = models.FloatField(blank=True, null=True)
-
-    comuna = models.CharField(max_length=100, blank=True, null=True)
-    tipo_construccion = models.CharField(max_length=100, blank=True, null=True)
-    altura = models.CharField(max_length=100, blank=True, null=True)
-    candado_bt = models.CharField(max_length=100, blank=True, null=True)
-    condiciones_acceso = models.TextField(blank=True, null=True)
-    claves = models.TextField(blank=True, null=True)
-    llaves = models.TextField(blank=True, null=True)
-    cantidad_llaves = models.CharField(max_length=255, blank=True, null=True)
-    observaciones_generales = models.TextField(blank=True, null=True)
-    zonas_conflictivas = models.TextField(blank=True, null=True)
-    alarmas = models.TextField(blank=True, null=True)
-    guardias = models.TextField(blank=True, null=True)
-    nivel = models.IntegerField(blank=True, null=True)
-    descripcion = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.nombre or self.id_sites
+from django.db import models
+from django.conf import settings
 
 
-class ServicioCotizado(models.Model):
-    ESTADOS = [
-        ('cotizado', 'Cotizado'),
-        ('aprobado_pendiente', 'Aprobada, pendiente por asignar'),
-        ('asignado', 'Técnicos asignados'),
-        ('en_progreso', 'En progreso'),
-        ('finalizado_trabajador', 'Finalizado por técnico'),
-        ('rechazado_supervisor', 'Rechazado por supervisor'),
-        ('aprobado_supervisor', 'Aprobado por supervisor'),
-        ('informe_subido', 'Informe cargado'),
-        ('finalizado', 'Finalizado'),
-    ]
-
-    du = models.CharField(max_length=20, blank=True, unique=True)
-    id_claro = models.CharField(max_length=100, blank=True, null=True)
-    region = models.CharField(max_length=100, blank=True, null=True)
-    mes_produccion = models.CharField(max_length=20)
-    id_new = models.CharField(max_length=100, blank=True, null=True)
-    detalle_tarea = models.TextField()
-    monto_cotizado = models.DecimalField(max_digits=12, decimal_places=2)
-    monto_mmoo = models.DecimalField(
-        max_digits=12, decimal_places=2, blank=True, null=True)
-
-    estado = models.CharField(
-        max_length=50, choices=ESTADOS, default='cotizado')
-
-    creado_por = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
-        null=True,
-        related_name='servicios_creados'
-    )
-
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-
-    trabajadores_asignados = models.ManyToManyField(
-        settings.AUTH_USER_MODEL,
-        blank=True,
-        related_name='servicios_asignados',
-        limit_choices_to={'rol': 'usuario'},
-        verbose_name='Técnicos asignados'
-    )
-
-    pm_aprueba = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='cotizaciones_aprobadas_pm',
-        verbose_name="Aprobado por PM"
-    )
-
-    tecnico_aceptado = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='servicios_aceptados_tecnico',
-        verbose_name="Aceptado por técnico"
-    )
-
-    tecnico_finalizo = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='servicios_finalizados_tecnico',
-        verbose_name="Finalizado por técnico"
-    )
-
-    supervisor_aprobo = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='servicios_aprobados_supervisor',
-        verbose_name="Aprobado por supervisor"
-    )
-
-    fecha_aprobacion_supervisor = models.DateTimeField(
-        null=True,
-        blank=True,
-        verbose_name="Fecha aprobación del supervisor"
-    )
-
-    supervisor_rechazo = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='servicios_rechazados_supervisor',
-        verbose_name="Rechazado por supervisor"
-    )
-
-    supervisor_asigna = models.ForeignKey(
-        CustomUser,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name='servicios_asignados_como_supervisor'
-    )
-
-    usuario_informe = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        null=True, blank=True,
-        on_delete=models.SET_NULL,
-        related_name='informes_cargados',
-        verbose_name='Usuario que cargó el informe'
-    )
-
-    informe = models.FileField(  # 👈 NUEVO CAMPO
-        upload_to=upload_to,
-        blank=True,
-        null=True,
-        verbose_name="Informe del servicio"
-    )
-
-    motivo_rechazo = models.TextField(blank=True, null=True)
+class PrecioActividadTecnico(models.Model):
+    tecnico = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ciudad = models.CharField(max_length=100)
+    proyecto = models.CharField(max_length=200)
+    codigo = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    unidad_medida = models.CharField(max_length=20)
+    precio_tecnico = models.DecimalField(max_digits=10, decimal_places=2)
+    precio_empresa = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_creacion = models.DateField(auto_now_add=True)
 
     class Meta:
-        managed = True
-
-    def save(self, *args, **kwargs):
-        if not self.du:
-            ultimo = ServicioCotizado.objects.exclude(
-                du='').order_by('-du').first()
-            if ultimo and ultimo.du.isdigit():
-                nuevo = str(int(ultimo.du) + 1).zfill(8)
-            else:
-                nuevo = '00000001'
-
-            while ServicioCotizado.objects.filter(du=nuevo).exists():
-                nuevo = str(int(nuevo) + 1).zfill(8)
-
-            self.du = nuevo
-
-        super().save(*args, **kwargs)
+        unique_together = ('tecnico', 'ciudad', 'proyecto', 'codigo')
+        verbose_name = 'Precio por Actividad'
+        verbose_name_plural = 'Precios por Actividad'
 
     def __str__(self):
-        return f"DU {self.du} - {self.id_claro}"
+        return f'{self.codigo} - {self.tecnico} - {self.ciudad}'
