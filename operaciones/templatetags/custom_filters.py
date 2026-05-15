5d1ed7b0582ad57b3c03a811a48425a27f59fcf5
luisgil42 <gilmoyaluisenrique@gmail.com>
2026-05-15 12:02:39 -0400

    Resolviendo problema de Filtro 1

--- operaciones/templatetags/custom_filters.py
+++ operaciones/templatetags/custom_filters.py
@@ -3,6 +3,7 @@ from django import template
 register = template.Library()
 
 
+
 @register.filter
 def miles(value):
     try:
@@ -83,3 +84,23 @@ def get_item(d, k):
         return d.get(k)
     except Exception:
         return None
+
+
+@register.filter
+def dictget(dictionary, key):
+    """
+    Allows dictionary access by dynamic key in templates.
+
+    Usage:
+        {{ my_dict|dictget:key }}
+    """
+    if dictionary is None:
+        return None
+
+    try:
+        return dictionary.get(key)
+    except AttributeError:
+        try:
+            return dictionary[key]
+        except Exception:
+            return None
