c79e284a52ddbf3b11f6813c926816a7f6dcd19e
luisgil42 <gilmoyaluisenrique@gmail.com>
2026-09-22 11:13:30 -0300

    arreglo del modal 1

--- hyperlink_networks/settings/dev.py
+++ hyperlink_networks/settings/dev.py
@@ -51,7 +51,7 @@ INTERNAL_IPS = ['127.0.0.1']
 
 # ==============================
 
-
+"""
 DATABASE_URL = os.environ.get("DATABASE_URL", "").strip()
 
 if DATABASE_URL:
@@ -96,7 +96,7 @@ DATABASES = {
         },
     }
 }
-"""
+
 # ==============================
 # IA
 # ==============================
